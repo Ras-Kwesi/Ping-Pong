8ab8c66167fe4198dd9750cf11c2b2cf0818adde
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-07-13 17:54:22 +0300

    gh-pages made and scripts.js pushed

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,10 @@
 
 
 
-
-
-
-
-
 // User-Interface Logic
 $(document).ready(function() {
   $("form#inputentry").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($(""))
+    var userInput = parseInt($("#numberentry"))
   })
 })
